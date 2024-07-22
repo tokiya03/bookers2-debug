6ce74a6b6118d8ef0f301dfc8466c834c29c26b4
tokiya03 <h.tokiya03@gmail.com>
2024-07-22 17:25:03 +0900

    [Finish] counting page views

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,9 @@ class BooksController < ApplicationController
     @new_book = Book.new
     @book_comment = BookComment.new
     @book_comments = @book.book_comments
+    unless ViewCount.find_by(user_id: current_user.id, book_id: @book.id)
+      current_user.view_counts.create(book_id: @book.id)
+    end
   end
 
   def index
